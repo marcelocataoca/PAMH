bc9daba963e352dfadae690e3169e22ce8529f5e
Marcelo Kenji <marcelo.cataoca@hotmail.com>
2017-05-16 01:55:18 -0300

    Add files via upload

---
+++ Sensores/ProjetosExemplo/06_SimpleTracker/www/js/app.js
@@ -0,0 +1,95 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services','ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+ .controller('GeoCtrl', function ($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
+   $scope.startGPS = (function () {
+      $ionicLoading.show({
+        template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+      });
+
+      var posOptions = {
+        enableHighAccuracy: true,
+        timeout: 50000,
+        maximumAge: 0
+      };
+
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+        console.log("Latitude: "+position.coords.latitude + "  Longitude: "+position.coords.longitude);
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        var myLatlng = new google.maps.LatLng(lat, long);
+
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        $scope.map = map;
+        $ionicLoading.hide();
+
+      }, function (err) {
+        $ionicLoading.hide();
+        console.log(err);
+      });
+    })
+
+    //Cordenadas inseridas
+     $scope.findLocal = (function () {
+      $ionicLoading.show({
+        template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+      });
+
+      var posOptions = {
+        enableHighAccuracy: true,
+        timeout: 50000,
+        maximumAge: 0
+      };
+
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(function () {
+        var lat = $scope.latitude;
+        var long = $scope.longitude;
+
+        var myLatlng = new google.maps.LatLng(lat, long);
+
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        $scope.map = map;
+        $ionicLoading.hide();
+
+      }, function (err) {
+        $ionicLoading.hide();
+        console.log(err);
+      });
+    })
+  })
